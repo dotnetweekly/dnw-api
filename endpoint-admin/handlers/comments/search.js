128029e0f9739a674452332afd0d51d7f7ce1dc8
Bill Stavroulakis <bstavroulakis@mesosphere.io>
2017-11-22 03:42:04 -0500

    comment and newsletter endpoints

--- endpoint-admin/handlers/comments/search.js
+++ endpoint-admin/handlers/comments/search.js
@@ -3,6 +3,8 @@ var LinkComment = require("../../../db/models/comment.model");
 const search = function(req, callback) {
   var query = LinkComment.find({});
 
+  query.populate("user").populate("link");
+
   query.exec(function(err, data) {
     if (err) {
       callback.onError([]);
