512902cdb007d4c2ca95cf84d4d434b507c4f7e1
bstavroulakis <bstavroulakis@gmail.com>
2018-01-24 22:32:19 -0800

    add environment variables

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -1,3 +1,13 @@
+for /d %%D in (%DEPLOYMENT_TARGET%\*) do (
+    if not "%%~nxD" == "node_modules" (
+        del /S /Q "%%~D"
+        IF "%ERRORLEVEL%" NEQ "0" goto error
+    )
+)
+for %%D in (%DEPLOYMENT_TARGET%\*) do (
+    del /S /Q "%%~D"
+    IF "%ERRORLEVEL%" NEQ "0" goto error
+)
 
 xcopy /d %DEPLOYMENT_SOURCE%\* %DEPLOYMENT_TARGET%\ /s /i
 
