c64d4e081d6a076131079b4420b993a7e217a1da
bstavroulakis <bstavroulakis@gmail.com>
2018-01-24 21:42:11 -0800

    add environment variables

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
-const newsletterDomain = 'http://localhost:6100/';
+const newsletterDomain = process.env.NEWSLETTER_DOMAIN || 'http://localhost:6100/';
 
 module.exports = {
 	auth: {
 		secret: 'secret'
 	},
 	database: {
-		local: 'mongodb://127.0.0.1:27017/dnwdb'
+		local: process.env.DB_CONNECTION || 'mongodb://127.0.0.1:27017/dnwdb'
 	},
 	newsletterDomain: newsletterDomain
 };
