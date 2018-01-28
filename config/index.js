344934e992b50b0980b882b625da945abec2bf6d
bstavroulakis <bstavroulakis@gmail.com>
2018-01-27 23:59:20 -0800

    add secret

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const newsletterDomain = process.env.NEWSLETTER_DOMAIN || 'http://localhost:6100
 
 module.exports = {
 	auth: {
-		secret: 'secret'
+		secret: process.env.SECRET || 'secret'
 	},
 	database: {
 		local: process.env.DB_CONNECTION || 'mongodb://127.0.0.1:27017/dnwdb'
