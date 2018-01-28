2410b1076d75c5dbc31d7eca6f5582e2b550614d
bstavroulakis <bstavroulakis@gmail.com>
2018-01-28 01:59:19 -0800

    change api domain

--- config/index.js
+++ config/index.js
@@ -5,5 +5,5 @@ module.exports = {
 	database: {
 		local: process.env.DB_CONNECTION || 'mongodb://127.0.0.1:27017/dnwdb'
 	},
-	newsletterDomain: 'http://localhost:6100/'
+	newsletterDomain: process.env.NEWSLETTER_DOMAIN || 'http://localhost:6100/'
 };
