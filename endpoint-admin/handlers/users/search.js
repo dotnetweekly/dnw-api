b53f87b55ab3e7b0ddd801fff8007e6346c7d444
bstavroulakis <bstavroulakis@gmail.com>
2018-02-19 13:04:21 -0800

    fix user search

--- endpoint-admin/handlers/users/search.js
+++ endpoint-admin/handlers/users/search.js
@@ -3,14 +3,21 @@ const User = require("../../../db/models/user.model");
 
 const search = function(req, callback) {
   const page = sanitize(req.query) ? sanitize(req.query.page) || 1 : 1;
-  const email = sanitize(req.params.email);
+  const email = sanitize(req.query.email) || "";
   const pageChunk = 12;
+
+  const searchQuery = {};
+
+  if (email) {
+    const regexSearch = new RegExp(`.*?${email}.*?`);
+    searchQuery.email = { $regex: regexSearch, $options: 'i' }
+  }
 
   User.count({}, function(err, totalItems){
 
     const pages = Math.ceil(parseFloat(totalItems) / pageChunk);
     const skipItems = (page - 1) * pageChunk;
-    var query = User.find({})
+    var query = User.find(searchQuery)
     .limit(pageChunk)
     .skip(skipItems)
     .sort({createdOn: "desc"});
