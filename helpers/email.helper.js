830298555b3296215b142a148159582f1bb4c827
bstavroulakis <bstavroulakis@gmail.com>
2018-02-20 01:56:10 -0800

    fix date range

--- helpers/email.helper.js
+++ helpers/email.helper.js
@@ -7,7 +7,7 @@ const emailHelper = {
 
     newHtml = newHtml.replace(/(\${USER_UNSUBSCRIBE})/gim, `${config.clientDomain}unsubscribe/${user.keyUnsubscribe}`);
     newHtml = newHtml.replace(/(\${USER_USERNAME})/gim, user.username);
-    newHtml = newHtml.replace(/(\${USER_RESET})/gim, `${config.clientDomain}resetPassword/${user.resetPassword}`);
+    newHtml = newHtml.replace(/(\${USER_RESET})/gim, `${config.clientDomain}forgot-password/${user.resetPassword}`);
     newHtml = newHtml.replace(/(\${USER_FISTNAME})/gim, user.firstName);
     newHtml = newHtml.replace(/(\$DOMAIN_CLIENT)/gim, config.clientDomain);
 
