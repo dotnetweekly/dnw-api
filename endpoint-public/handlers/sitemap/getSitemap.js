ceb2eaf9e74a37b05837dde509bebf477cc04da3
bstavroulakis <bstavroulakis@gmail.com>
2018-02-25 23:20:47 -0800

    add sitemap

--- endpoint-public/handlers/sitemap/getSitemap.js
+++ endpoint-public/handlers/sitemap/getSitemap.js
@@ -4,64 +4,35 @@ var Link = require("../../../db/models/link.model");
 var CalendarHelper = require("../../../helpers/calendar.helper");
 
 const getSitemap = function(req, callback) {
+  let weekUrls = [];
+  let firstYear = 2012;
+  let firstWeek = 19;
 
-  var query = Link.find({}, [
-    "title",
-    "createdOn",
-    "slug",
-    "category",
-    "tags"
-  ]);
-  
-  query
-    .populate("user", "username")
-    .sort({ createdOn: -1 });
-
-  query.exec(function(err, data) {
-
-    let linkUrls = [];
-    for (var i = 0; i < data.length; i++) {
-      let linkUrl = `/${data[i].category}/${data[i].slug}`;
-      linkUrl = linkUrl.replace(/\/$/, "");
-      linkUrls.push({ url: linkUrl });
+  const now = new Date(Date.now());
+  const currentWeek = CalendarHelper.getWeek(now);
+  const currentYear = now.getFullYear();
+  while (firstYear < currentYear || (firstYear == currentYear && firstWeek <= currentWeek)) {
+    weekUrls.push(`/week/${firstWeek}/year/${firstYear}`);
+    firstWeek++;
+    if (firstWeek > CalendarHelper.weeksInYear(firstYear)) {
+      firstWeek = 1;
+      firstYear++;
     }
+  }
 
-    for (var i = 0; i < data.length; i++) {
-      let linkUrl = `/${data[i].category}/${data[i].slug}`;
-      linkUrl = linkUrl.replace(/\/$/, "");
-      linkUrls.push({ url: linkUrl });
-    }
+  const totalLinks = linkUrls.concat(weekUrls)
+    .concat(`/about`)
+    .concat(`/newsletters`)
 
-    let weekUrls = [];
-    let firstYear = 2012;
-    let firstWeek = 19;
-
-    const now = new Date(Date.now());
-    const currentWeek = CalendarHelper.getWeek(now);
-    const currentYear = now.getFullYear();
-    while (firstYear < currentYear || (firstYear == currentYear && firstWeek <= currentWeek)) {
-      weekUrls.push(`/week/${firstWeek}/year/${firstYear}`);
-      firstWeek++;
-      if (firstWeek > CalendarHelper.weeksInYear(firstYear)) {
-        firstWeek = 1;
-        firstYear++;
-      }
-    }
-
-    const totalLinks = linkUrls.concat(weekUrls)
-      .concat(`/about`)
-      .concat(`/newsletters`)
-
-    const sitemap = sm.createSitemap ({
-      hostname: config.clientDomain,
-      cacheTime: 600000,
-      urls: totalLinks
-    });
-
-    callback.onSuccess("", "", "", sitemap.toString());
-
-    return;
+  const sitemap = sm.createSitemap ({
+    hostname: config.clientDomain,
+    cacheTime: 600000,
+    urls: totalLinks
   });
+
+  callback.onSuccess("", "", "", sitemap.toString());
+
+  return;
 };
 
 module.exports = getSitemap;
