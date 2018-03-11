870d2505264b84ef13248a487323e3d27c70f4f0
bstavroulakis <bstavroulakis@gmail.com>
2018-03-10 23:40:54 -0800

    add cache package

--- app.js
+++ app.js
@@ -1,52 +1,58 @@
-var express = require("express");
+var express = require('express');
 const sanitize = require('mongo-sanitize');
-var path = require("path");
-var logger = require("morgan");
-var bodyParser = require("body-parser");
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
-var routes = require("./routes");
-var connect = require("./db/connect");
+var routes = require('./routes');
+var connect = require('./db/connect');
+const LRU = require('lru-cache');
+
+const microCache = LRU({
+	max: 1500,
+	maxAge: 60000
+});
 
 var app = express();
 
-app.use(logger("dev")); /* 'default', 'short', 'tiny', 'dev' */
+app.use(logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 function allowCrossDomain(req, res, next) {
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
 
-  var origin = sanitize(req.headers.origin);
-  if (origin) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.setHeader("Access-Control-Allow-Headers", "content-type, Authorization");
+	var origin = sanitize(req.headers.origin);
+	if (origin) {
+		res.setHeader('Access-Control-Allow-Origin', origin);
+	}
+	res.setHeader('Access-Control-Allow-Headers', 'content-type, Authorization');
 
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
+	if (req.method === 'OPTIONS') {
+		res.sendStatus(200);
+	} else {
+		next();
+	}
 }
 
 app.use(allowCrossDomain);
 
-app.use("/api", routes);
+app.use('/api', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  console.log(err);
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+	console.log(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 app.use(function(err, req, res, next) {
-  console.log(err);
-  res.status(err.status || 500).send({
-    success: false,
-    message: err.message
-  });
+	console.log(err);
+	res.status(err.status || 500).send({
+		success: false,
+		message: err.message
+	});
 });
 
 module.exports = app;
