33db2ace2475ccafe5b4a4d8cbe3e947c8a8b3e8
Bill Stavroulakis <bstavroulakis@gmail.com>
2018-04-09 19:39:52 -0700

    newsletter updates

--- email.js
+++ email.js
@@ -4,25 +4,25 @@ const client = new SparkPost(sparkPostKey);
 
 class Email {
   send(users, subject, html, campaign_id = "") {
-    // client.transmissions
-    // 	.send({
-    // 		options: {
-    // 			sandbox: false
-    // 		},
-    // 		content: {
-    // 			from: { name: 'dotNET Weekly', email: 'info@dotnetweekly.com' },
-    // 			subject: subject,
-    // 			html: html
-    // 		},
-    // 		recipients: users,
-    // 		campaign_id: campaign_id
-    // 	})
-    // 	.then(data => {
-    // 		console.log(data);
-    // 	})
-    // 	.catch(err => {
-    // 		console.log(err);
-    // 	});
+    client.transmissions
+      .send({
+        options: {
+          sandbox: false
+        },
+        content: {
+          from: { name: "dotNET Weekly", email: "info@dotnetweekly.com" },
+          subject: subject,
+          html: html
+        },
+        recipients: users,
+        campaign_id: campaign_id
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 
