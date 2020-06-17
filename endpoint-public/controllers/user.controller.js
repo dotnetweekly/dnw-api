9020dd08c8a4979388f81d074de6268618a08e01
Bill Stavroulakis <bill.stavroulakis@getcruise.com>
2020-06-16 19:20:51 -0700

    fix unsubscribe

--- endpoint-public/controllers/user.controller.js
+++ endpoint-public/controllers/user.controller.js
@@ -1,120 +1,110 @@
-const BaseController = require('./base.controller');
-const UserHandler = require('../handlers/user');
+const BaseController = require("./base.controller");
+const UserHandler = require("../handlers/user");
 const RecaptchaError = require("../../error/recaptcha");
 
 class UserController extends BaseController {
-	constructor() {
-		super();
-		this._userHandler = new UserHandler();
-	}
+  constructor() {
+    super();
+    this._userHandler = new UserHandler();
+  }
 
-	profile(req, res, next) {
-		const response = this._responseManager.getResponseHandler(req, res);
-    
-		if (response) {
-			this._userHandler.profile(response);
-		}
-	}
-	
-	saveProfile(req, res, next) {
-		const response = this._responseManager.getResponseHandler(req, res);
+  profile(req, res, next) {
+    const response = this._responseManager.getResponseHandler(req, res);
+
+    if (response) {
+      this._userHandler.profile(response);
+    }
+  }
+
+  saveProfile(req, res, next) {
+    const response = this._responseManager.getResponseHandler(req, res);
 
     if (!req.recaptcha || (req.recaptcha && req.recaptcha.error)) {
       const recaptchaError = new RecaptchaError();
       response.onSuccess({
-        errors: [{field: "recaptcha", error: recaptchaError.message}]
+        errors: [{ field: "recaptcha", error: recaptchaError.message }],
       });
 
       return;
     }
 
-		if (response) {
-			this._userHandler.saveProfile(req, response);
-		}
-	}
+    if (response) {
+      this._userHandler.saveProfile(req, response);
+    }
+  }
 
-	submitted(req, res, next) {
-		const response = this._responseManager.getResponseHandler(req, res, true);
+  submitted(req, res, next) {
+    const response = this._responseManager.getResponseHandler(req, res, true);
 
-		if (response) {
-			this._userHandler.submitted(req, response);
-		}
-	}
+    if (response) {
+      this._userHandler.submitted(req, response);
+    }
+  }
 
-	upvotes(req, res, next) {
-		const response = this._responseManager.getResponseHandler(req, res, true);
+  upvotes(req, res, next) {
+    const response = this._responseManager.getResponseHandler(req, res, true);
 
-		if (response) {
-			this._userHandler.upvotes(req, response);
-		}
-	}
+    if (response) {
+      this._userHandler.upvotes(req, response);
+    }
+  }
 
-	comments(req, res, next) {
-		const response = this._responseManager.getResponseHandler(req, res, true);
+  comments(req, res, next) {
+    const response = this._responseManager.getResponseHandler(req, res, true);
 
-		if (response) {
-			this._userHandler.comments(req, response);
-		}
-	}
+    if (response) {
+      this._userHandler.comments(req, response);
+    }
+  }
 
-	userCount(req, res, next) {
-		const response = this._responseManager.getResponseHandler(req, res, true);
+  userCount(req, res, next) {
+    const response = this._responseManager.getResponseHandler(req, res, true);
 
-		if (response) {
-			this._userHandler.userCount(req, response);
-		}
-	}
-	
-	updateEmail(req, res, next) {
-		const response = this._responseManager.getResponseHandler(req, res, true);
+    if (response) {
+      this._userHandler.userCount(req, response);
+    }
+  }
+
+  updateEmail(req, res, next) {
+    const response = this._responseManager.getResponseHandler(req, res, true);
 
     if (!req.recaptcha || (req.recaptcha && req.recaptcha.error)) {
       const recaptchaError = new RecaptchaError();
       response.onSuccess({
-        errors: [{field: "recaptcha", error: recaptchaError.message}]
+        errors: [{ field: "recaptcha", error: recaptchaError.message }],
       });
 
       return;
     }
-    
-		if (response) {
-			this._userHandler.updateEmail(req, response);
-		}
-	}
-	
-	forgotPassword(req, res, next) {
-		const response = this._responseManager.getResponseHandler(req, res, true);
+
+    if (response) {
+      this._userHandler.updateEmail(req, response);
+    }
+  }
+
+  forgotPassword(req, res, next) {
+    const response = this._responseManager.getResponseHandler(req, res, true);
 
     if (!req.recaptcha || (req.recaptcha && req.recaptcha.error)) {
       const recaptchaError = new RecaptchaError();
       response.onSuccess({
-        errors: [{field: "recaptcha", error: recaptchaError.message}]
+        errors: [{ field: "recaptcha", error: recaptchaError.message }],
       });
 
       return;
     }
-    
-		if (response) {
-			this._userHandler.forgotPassword(req, response);
-		}
-	}
-	
-	unsubscribe(req, res, next) {
-		const response = this._responseManager.getResponseHandler(req, res, true);
 
-    if (!req.recaptcha || (req.recaptcha && req.recaptcha.error)) {
-      const recaptchaError = new RecaptchaError();
-      response.onSuccess({
-        errors: [{field: "recaptcha", error: recaptchaError.message}]
-      });
-
-      return;
+    if (response) {
+      this._userHandler.forgotPassword(req, response);
     }
-    
-		if (response) {
-			this._userHandler.unsubscribe(req, response);
-		}
-	}
+  }
+
+  unsubscribe(req, res, next) {
+    const response = this._responseManager.getResponseHandler(req, res, true);
+    if (response) {
+      this._userHandler.unsubscribe(req, response);
+    }
+  }
 }
 
 module.exports = UserController;
