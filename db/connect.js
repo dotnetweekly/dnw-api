dbb44ff4a4c0609e9e5c16afdd64473d70cb8fde
Bill Stavroulakis <bill.stavroulakis@getcruise.com>
2020-06-16 19:01:55 -0700

    mongoose

--- db/connect.js
+++ db/connect.js
@@ -2,4 +2,7 @@ var mongoose = require("mongoose");
 var config = require("../config/index");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.database.local);
+mongoose.connect(config.database.local, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
